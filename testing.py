e0caca2b08702c4e12f47e0458e88113423ee51b
sai-cs-stud <srm275@scarletmail.rutgers.edu>
2021-04-18 01:14:09 -0400

    plotly in tester

--- testing.py
+++ testing.py
@@ -1,6 +1,25 @@
 
 from main import budgetGrouping
+import plotly.express as px
 budge = budgetGrouping()
 grouped = budge.reformat()
 
 print(grouped)
+#plotf = px.data.stocks()
+# Plot Expenses
+fig = px.line(grouped, x='Date', y='Expenses($)')
+fig.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+fig.show()
+# Using input expense sheet(above) and input income sheet, use same method 
+# to plot a new df in which each day is a continous sum
